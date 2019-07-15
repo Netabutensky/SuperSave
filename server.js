bacb171d3a1b9ed16f36abb3d7da35e63308b598
neta butensky <netabutensky@gmail.com>
2019-07-15 17:23:09 +0300

    first step

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require( 'express' )
+const app = express()
+const port = 8080
+const path = require( 'path' )
+
+const bodyParser = require( 'body-parser' )
+const mongoose = require( 'mongoose' )
+
+const router = require('./server/routes/api')
+
+app.use( bodyParser.json() )
+app.use( bodyParser.urlencoded( { extended: false } ) )
+
+app.use( express.static( path.join( __dirname, 'dist' ) ) )
+app.use( '/', router )
+
+mongoose.connect('mongodb://localhost/hackathonDB', { useNewUrlParser: true } ).then( () => {
+   app.listen( port, () => console.log( `Running server on port ${ port }` ) )
+} )
+
