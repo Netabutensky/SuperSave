0c173489a89edda4e40ff63304beb6761157d7be
yuval schwartz <yuvals249@gmail.com>
2019-07-16 21:11:36 +0300

    first router

--- server/model/city.js
+++ server/model/city.js
@@ -1,13 +1,21 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const citiesSchema = new Schema({
+const storeSchema = new Schema ({
    store_name: String,
    store_id: Number,
    store_address: String,
+})
+
+const citiesSchema = new Schema({
+   name: String,
+   city_id: Number,
+   city_stores: [storeSchema],
 
 })
+
 
 const City = mongoose.model('city', citiesSchema)
 
 module.exports = City
+
--- server/routes/api.js
+++ server/routes/api.js
@@ -2,19 +2,45 @@ const express = require('express')
 const router = express.Router()
 const request = require('request-promise')
 const API_KEY = '2884fae2abf12546ec3d81ea109fa6d284a6b4bc'
-// const Product = require('../model/product')
+const City = require('../model/city')
+const User = require('../model/user')
 
 router.get('/city/:cityName', async (req, res) => {
-   const stores = []
-   const cityName = req.params.cityName
-   request.post(`https://api.superget.co.il/?api_key=${API_KEY}&action=GetCityByName&city_name=${cityName}`, async function(err,response) {
-       const cityId = response.city_id
-       const cityStores = await request.post(`https://api.superget.co.il/?api_key=${YOUR_API_KEY}&action=GetStoresByCityID&city_id=${cityId}`)
-       cityStores = JSON.parse(response)
-       cityStores =cityStores.map(c => ({storeName: c.chain_name , storeAdress: c.store_address}))
-       stores.push(cityStores)
-   })
-   res.send(stores)
+    const cityName = req.params.cityName
+    let cityId
+    let response = await request.post(`https://api.superget.co.il/?api_key=2884fae2abf12546ec3d81ea109fa6d284a6b4bc&action=GetCities`)
+    response = JSON.parse(response)
+    for (let c of response)
+    {
+        if (c.city_name == cityName)
+        {
+            cityId = c.city_id
+            break
+        }
+    }
+    let cityStores = await request.post(`https://api.superget.co.il/?api_key=2884fae2abf12546ec3d81ea109fa6d284a6b4bc&action=GetStoresByCityID&city_id=${cityId}`)
+    cityStores = JSON.parse(cityStores)
+    cityStores =cityStores.map(c => ({store_name: c.chain_name , store_id : c.store_id, store_address: c.store_address}))    
+    const stores = []
+    const cs = []
+
+    for(let i = 0; i < cityStores.length; i++)
+    {
+        if (!stores.includes(cityStores[i].store_name))
+        {
+            stores.push(cityStores[i].store_name)
+            cs.push(cityStores[i])
+        }
+    }
+    
+    let city1 = new City({name: cityName, city_id: cityId, city_stores:cs})
+    city1.save()
+    res.end() 
+})
+
+router.get('/city/:product', async (req, res) => {
+    const product = req.params.product
+
 
 })
 
